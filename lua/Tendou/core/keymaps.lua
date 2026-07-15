6a2c3f620683930baaa659ae0d4038a504e7612e
DefinitelyNotJay <jjbrathers3@gmail.com>
2026-07-15 12:01:43 +0700

    fix space + s flash nvim

--- lua/Tendou/core/keymaps.lua
+++ lua/Tendou/core/keymaps.lua
@@ -32,7 +32,7 @@ vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 vim.keymap.set("n", "x", '"_x', opts)
 
 -- Replace the word cursor is on globally
-vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], { desc = "Replace word cursor is on globally" })
+vim.keymap.set("n", "<leader>sr", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], { desc = "Replace word cursor is on globally" })
 -- Executes shell command from in here making file executable
 vim.keymap.set("n", "<leader>X", "<cmd>!chmod +x %<CR>", { silent = true, desc = "makes file executable" })
 
--- lua/Tendou/plugins/flash.lua
+++ lua/Tendou/plugins/flash.lua
@@ -5,8 +5,8 @@ return {
   opts = {},
   -- เราจะตั้งค่า Keymap ไว้ในนี้เลย เพื่อให้ Lazy.nvim โหลดปลั๊กอินนี้เมื่อกดปุ่มพวกนี้เท่านั้น
   keys = {
-    { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-    { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+    { "<leader>s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+    { "<leader>S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
     { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
     { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
     { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
