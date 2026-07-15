ab1ed0c1b744ca89fab3a6b265b2746c1b99b375
DefinitelyNotJay <jjbrathers3@gmail.com>
2026-07-15 18:54:47 +0700

    add pyright

--- lua/Tendou/plugins/lsp.lua
+++ lua/Tendou/plugins/lsp.lua
@@ -13,14 +13,15 @@ return {
         "tailwindcss", -- Tailwind CSS for Next.js
         "eslint", -- Linter
         "html", 
-        "cssls" 
+        "cssls",
+        "pyright", -- Python
       },
     })
 
     require("lspconfig") -- Load nvim-lspconfig defaults
     local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-    local servers = { "ts_ls", "tailwindcss", "eslint", "html", "cssls" }
+    local servers = { "ts_ls", "tailwindcss", "eslint", "html", "cssls", "pyright" }
     for _, server in ipairs(servers) do
       vim.lsp.config(server, {
         capabilities = capabilities,
